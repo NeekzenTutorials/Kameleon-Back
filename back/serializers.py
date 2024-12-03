d7b009f7d9bd66470cb55baab69ecfe24ee50798
Neekzen <nathanfourny.pro@gmail.com>
2024-12-03 15:36:01 +0100

    feat: create a link to get user profile picture

--- back/serializers.py
+++ back/serializers.py
@@ -14,6 +14,12 @@ class UserDetailSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'profile_picture', 'rank', 'created_at']
         read_only_fields = ['id', 'rank', 'created_at']
+        
+    def get_profile_picture(self, obj):
+        request = self.context.get('request')
+        if obj.profile_picture:
+            return request.build_absolute_uri(obj.profile_picture.url)
+        return None
 
 class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
--- kameleon_back/settings.py
+++ kameleon_back/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
