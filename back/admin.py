dcd8e65f10267c95f8ef031b8069505a346e1d5e
Neekzen <nathanfourny.pro@gmail.com>
2024-12-04 10:47:54 +0100

    feat: remove seed data and update pannel admin

--- back/admin.py
+++ back/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from back.models import Rank
 
-# Register your models here.
+@admin.register(Rank)
+class RankAdmin(admin.ModelAdmin):
+    list_display = ('rank_name', 'rank_image')
+    search_fields = ('rank_name',) 
+    list_filter = ('rank_name',)
+
+    def image_preview(self, obj):
+        if obj.rank_image:
+            return format_html(f'<img src="{obj.rank_image.url}" style="height: 50px;"/>')
+        return "No image"
+    
+    image_preview.short_description = 'Preview'
