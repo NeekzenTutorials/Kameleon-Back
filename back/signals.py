9e5b5e3965ec210351288c049e0aad47db7c5ad5
Neekzen <neekzen.62@gmail.com>
2025-01-07 15:05:12 +0100

    fix: get riddle id properly

--- back/signals.py
+++ back/signals.py
@@ -7,5 +7,5 @@ def create_member_for_user(sender, instance, created, **kwargs):
     if created:
         member = Member.objects.create(user=instance)
         # Set all riddles in locked_riddles
-        riddles_to_lock = Riddle.objects.exclude(id=2) # Exclude the first riddle (id=2)
+        riddles_to_lock = Riddle.objects.exclude(riddle_id=2) # Exclude the first riddle (id=2)
         member.locked_riddles.set(riddles_to_lock)
