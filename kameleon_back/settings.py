9c6200bf5135a7a31d1cbc77e71c61d7102fd3a4
NeekzenTutorials <neekzen.62@gmail.com>
2025-01-08 19:10:54 +0100

    fix: ghost email credential

--- kameleon_back/settings.py
+++ kameleon_back/settings.py
@@ -39,9 +39,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ciphers.kameleon@gmail.com'
-EMAIL_HOST_PASSWORD = 'mjpc fzpy ylsi hzkg'
-DEFAULT_FROM_EMAIL = 'ciphers.kameleon@gmail.com'
+EMAIL_HOST_USER = env('EMAIL_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL = env('EMAIL_USER')
 BACKEND_URL = 'https://kameleonback.jrcan.dev'
 
 # Application definition
