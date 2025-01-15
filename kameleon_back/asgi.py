2e46e2db20fb60d06ba9a5deec3e14f9746b1a98
Neekzen <neekzen.62@gmail.com>
2025-01-15 09:33:45 +0100

    fix: django setup in asgi.py

--- kameleon_back/asgi.py
+++ kameleon_back/asgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kameleon_back.settings')
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
